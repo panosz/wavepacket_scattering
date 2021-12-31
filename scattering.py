2613f0edab64169037ba84201e8f98fdff963d19
panosz <panosz@gmail.com>
2021-12-31 13:20:10 +0200

    New class `RandomInitialConditionMaker`. Also wider wavepacket.

--- scattering.py
+++ scattering.py
@@ -8,43 +8,59 @@ rng = default_rng()
 
 def random(start, stop, num=50):
     width = stop - start
-
     return start + width * rng.random(num)
+
+
+class RandomInitialConditionMaker():
+    """
+    Makes random initial conditions uniformly distributed in momentum and time.
+
+    """
     
+    def __init__(self, wp, p_eps=1e-4):
+        self.wp=wp
+        self.x_wall = 5*wp.sigma
+        self.p_eps = p_eps
 
-def make_initial_points_positive(num):
-    p0 = random(1e-4, 5e-1, num=num)
-    x0 = np.full_like(p0, fill_value=-50.5)
+    def _make_initial_points(self, num, p_range, x_wall):
+        p0 = random(*p_range, num=num)
+        x0 = np.full_like(p0, fill_value=x_wall)
+        return np.column_stack((x0.ravel(), p0.ravel()))
 
-    return np.column_stack((x0.ravel(), p0.ravel()))
+    def make_initial_points(self, num, p_max):
+        pos = self._make_initial_points(
+            num,
+            p_range=(self.p_eps, p_max),
+            x_wall=-self.x_wall,
+        )
+        neg = self._make_initial_points(
+            num,
+            p_range=(-p_max, -self.p_eps),
+            x_wall=self.x_wall,
+        )
 
-def make_initial_points_negative(num):
-    p0 = random(-1e-4, -5e-1, num=num)
-    x0 = np.full_like(p0, fill_value=50.5)
+        return np.row_stack((pos,neg))
 
-    return np.column_stack((x0.ravel(), p0.ravel()))
+    def make_initial_times(self, num):
+        omega = self.wp.k * self.wp.vp
+        T = 2*np.pi/omega
+        return random(0,T,num)
+    
+    def make_initial_conditions(self, num, p_max):
+        points = self.make_initial_points(num, p_max)
+        times = self.make_initial_times(2*num)
 
-def make_initial_points(num):
-    pos = make_initial_points_positive(num)
-    neg = make_initial_points_negative(num)
-
-    return np.row_stack((pos,neg))
-
-
-def make_initial_times(wp, num):
-    omega = wp.k * wp.vp
-    T = 2*np.pi/omega
-    return random(0,T,num)
+        return points, times
 
 if __name__ == "__main__":
-    wp = WavePacket(A=1e-2, sigma=10, k=1, vp=0.01)
+    wp = WavePacket(A=1e-2, sigma=40, k=1, vp=0.01)
 
-    init_points = make_initial_points(10000)
-    init_times = make_initial_times(wp, init_points.shape[0])
+    icm = RandomInitialConditionMaker(wp)
+    init_points, init_times = icm.make_initial_conditions(num=10000, p_max=5e-1)
 
     scatterrer = wp.make_integrator(atol=1e-10, rtol=1e-10)
 
-    scat = np.row_stack([scatterrer.integrate(point,t_integr=(t, t+10000))
+    scat = np.row_stack([scatterrer.integrate(point,t_integr=(t, t+1000000))
                             for point, t in zip(init_points, init_times)])
 
     out=np.column_stack((init_points,scat))
