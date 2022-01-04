5957fd21abac832a4a81f483df31c133b26e89c2
panosz <panosz@gmail.com>
2022-01-04 12:50:58 +0200

    scattering.py. Improve styling.

--- scattering.py
+++ scattering.py
@@ -7,6 +7,7 @@ from wavepacket import WavePacket
 
 rng = default_rng()
 
+
 def random(start, stop, num=50):
     width = stop - start
     return start + width * rng.random(num)
@@ -17,9 +18,9 @@ class RandomInitialConditionMaker():
     Makes random initial conditions uniformly distributed in momentum and time.
 
     """
-    
+
     def __init__(self, wp, p_eps=1e-4):
-        self.wp=wp
+        self.wp = wp
         self.x_wall = 5*wp.sigma
         self.p_eps = p_eps
 
@@ -40,23 +41,24 @@ class RandomInitialConditionMaker():
             x_wall=self.x_wall,
         )
 
-        return np.row_stack((pos,neg))
+        return np.row_stack((pos, neg))
 
     def make_initial_times(self, num):
         omega = self.wp.k * self.wp.vp
         T = 2*np.pi/omega
-        return random(0,T,num)
-    
+        return random(0, T, num)
+
     def make_initial_conditions(self, num, p_max):
         points = self.make_initial_points(num, p_max)
         times = self.make_initial_times(2*num)
 
         return points, times
 
+
 def check_if_transmitted(scattering_result):
     "checks which particles maintained their direction after collision"
-    momenta = scattering_result[:,[1,3]]
-    return momenta[:,0] * momenta[:,1] > 0 
+    momenta = scattering_result[:, [1, 3]]
+    return momenta[:, 0] * momenta[:, 1] > 0
 
 
 def transmission_coeff(scattering_result):
@@ -65,26 +67,27 @@ def transmission_coeff(scattering_result):
 
 
 def average_p_and_transmission_coeff(scattering_result):
-    p_mean = np.mean(scattering_result[:,1])
+    p_mean = np.mean(scattering_result[:, 1])
     tr_coeff = transmission_coeff(scattering_result)
     return p_mean, tr_coeff
 
 
 def sort_scattering_result(scattering_result):
-    return scattering_result[scattering_result[:,1].argsort()]
+    return scattering_result[scattering_result[:, 1].argsort()]
+
 
 def theoretical_transmission_coeff_formula(p_i, wp):
-    return wp.vp * 2 * np.sqrt(2) *np.pi/(p_i-wp.vp)
+    return wp.vp * 2 * np.sqrt(2) * np.pi/(p_i-wp.vp)
+
 
 def theoretical_transmission_coeff_i(p_i, wp):
-    vp = wp.vp
     A = wp.A
     if p_i <= -np.sqrt(2 * A) or p_i >= np.sqrt(2*A):
         return 1.0
 
     if p_i <= 0.0:
         return 0
-    
+
     out = theoretical_transmission_coeff_formula(p_i, wp)
 
     out = min(1, out)
@@ -94,14 +97,13 @@ def theoretical_transmission_coeff_i(p_i, wp):
 
 
 def theoretical_transmission_coeff_corrected_i(p_i, wp):
-    vp = wp.vp
     A = wp.A
     if p_i <= -np.sqrt(2 * A) or p_i >= np.sqrt(2*A):
         return 1.0
 
     if p_i <= 0.0:
         return 0
-    
+
     out = theoretical_transmission_coeff_formula(p_i, wp)/(2*np.pi)
 
     out = min(1, out)
@@ -109,67 +111,71 @@ def theoretical_transmission_coeff_corrected_i(p_i, wp):
 
     return out
 
+
 def theoretical_transmission_coeff(p, wp):
-    return np.array([theoretical_transmission_coeff_i(pi, wp) 
+    return np.array([theoretical_transmission_coeff_i(pi, wp)
                      for pi in p])
 
 
 def theoretical_transmission_coeff_corrected(p, wp):
-    return np.array([theoretical_transmission_coeff_corrected_i(pi, wp) 
+    return np.array([theoretical_transmission_coeff_corrected_i(pi, wp)
                      for pi in p])
+
 
 if __name__ == "__main__":
     wp = WavePacket(A=1e-2, sigma=40, k=1, vp=0.015)
 
     icm = RandomInitialConditionMaker(wp)
-    init_points, init_times = icm.make_initial_conditions(num=10000, p_max=5e-1)
+    init_points, init_times = icm.make_initial_conditions(num=10000,
+                                                          p_max=5e-1)
 
     scatterrer = wp.make_integrator(atol=1e-10, rtol=1e-10)
 
-    scat = np.row_stack([scatterrer.integrate(point,t_integr=(t, t+1000000))
-                            for point, t in zip(init_points, init_times)])
+    scat = np.row_stack([scatterrer.integrate(point, t_integr=(t, t+1000000))
+                         for point, t in zip(init_points, init_times)])
 
-    out=sort_scattering_result(np.column_stack((init_points,scat)))
-
-    fig, ax = plt.subplots()
-    ax.plot(out[:,1], out[:,-1]-out[:,1],',k',alpha=0.2)
-    ax.set_aspect("equal")
-    ax.axvspan(xmin=np.sqrt(2 * wp.A)+wp.vp,
-             xmax=max(ax.get_xlim()),
-             alpha=0.2,
-    )
-    ax.axvspan(xmin=min(ax.get_xlim()),
-             xmax=-np.sqrt(2 * wp.A)+wp.vp,
-             alpha=0.2,
-    )
-    ax.axvspan(xmin=-np.sqrt(2 * wp.A)+wp.vp,
-             xmax=np.sqrt(2 * wp.A)+wp.vp,
-             alpha=0.2,
-             color='r')
+    out = sort_scattering_result(np.column_stack((init_points,
+                                                  scat)))
 
     fig, ax = plt.subplots()
-    ax.plot(out[:,1], out[:,-1]-out[:,1],',k',alpha=1)
+    ax.plot(out[:, 1], out[:, -1]-out[:, 1], ',k', alpha=0.2)
     ax.set_aspect("equal")
     ax.axvspan(xmin=np.sqrt(2 * wp.A)+wp.vp,
-             xmax=max(ax.get_xlim()),
-             alpha=0.2,
-    )
+               xmax=max(ax.get_xlim()),
+               alpha=0.2,
+               )
     ax.axvspan(xmin=min(ax.get_xlim()),
-             xmax=-np.sqrt(2 * wp.A)+wp.vp,
-             alpha=0.2,
-    )
+               xmax=-np.sqrt(2 * wp.A)+wp.vp,
+               alpha=0.2,
+               )
     ax.axvspan(xmin=-np.sqrt(2 * wp.A)+wp.vp,
-             xmax=np.sqrt(2 * wp.A)+wp.vp,
-             alpha=0.2,
-             color='r')
- 
+               xmax=np.sqrt(2 * wp.A)+wp.vp,
+               alpha=0.2,
+               color='r')
+
+    fig, ax = plt.subplots()
+    ax.plot(out[:, 1], out[:, -1]-out[:, 1], ',k', alpha=1)
+    ax.set_aspect("equal")
+    ax.axvspan(xmin=np.sqrt(2 * wp.A)+wp.vp,
+               xmax=max(ax.get_xlim()),
+               alpha=0.2,
+               )
+    ax.axvspan(xmin=min(ax.get_xlim()),
+               xmax=-np.sqrt(2 * wp.A)+wp.vp,
+               alpha=0.2,
+               )
+    ax.axvspan(xmin=-np.sqrt(2 * wp.A)+wp.vp,
+               xmax=np.sqrt(2 * wp.A)+wp.vp,
+               alpha=0.2,
+               color='r')
 
     fig, ax = plt.subplots()
     chunks = 100
-    p_av=[]
-    tr_c=[]
-    for sc_r in chunked(out,out.shape[0]//chunks):
-        sc_r=np.row_stack(sc_r)
+    p_av = []
+    tr_c = []
+    for sc_r in chunked(out,
+                        out.shape[0]//chunks):
+        sc_r = np.row_stack(sc_r)
         p_av_i, tr_c_i = average_p_and_transmission_coeff(sc_r)
         p_av.append(p_av_i)
         tr_c.append(tr_c_i)
@@ -180,10 +186,6 @@ if __name__ == "__main__":
 
     tr_theoretical = theoretical_transmission_coeff_corrected(p_av, wp)
 
-    ax.plot(p_av, tr_theoretical,'r--')
-
+    ax.plot(p_av, tr_theoretical, 'r--')
 
     plt.show()
-
-    
-
