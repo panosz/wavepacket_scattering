c800b281bce184758d8212ba537fc6dc3558aa90
panosz <panosz@gmail.com>
2022-01-27 14:46:44 +0200

    Ref. styling `wavepacket_c`

--- wavepacket_c.py
+++ wavepacket_c.py
@@ -1,39 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#  from wavepacket import PyWavePacket as WavePacket
 from wavepacket import WavePacket
-
-
-
-
 
 if __name__ == "__main__":
     wp = WavePacket(A=1e-2, sigma=10, k=1, vp=0.01)
 
     p0 = 12e-3
-    x0 = np.linspace(-50.5,-40, num=1000)
+    x0 = np.linspace(-50.5, -40, num=1000)
 
     scatterrer = wp.make_integrator(atol=1e-10, rtol=1e-10)
 
-    scat = np.column_stack([scatterrer.integrate(point=[xi, p0],t_integr=(0, 10000))
+    scat = np.column_stack([scatterrer.integrate(point=[xi, p0],
+                                                 t_integr=(0, 10000))
                             for xi in x0])
 
-    out=np.zeros((4,x0.size))
+    out = np.zeros((4, x0.size))
     out
-    out[0,:]=x0
-    out[1,:]=p0
-    out[2:,:]=scat
+    out[0, :] = x0
+    out[1, :] = p0
+    out[2:, :] = scat
 
     fig, ax = plt.subplots()
-    ax.plot(x0,scat[1,:],',k',alpha=0.5)
-    ax.axhline(p0,c="k", linestyle='--',alpha=0.5)
+    ax.plot(x0, scat[1, :], ',k', alpha=0.5)
+    ax.axhline(p0, c="k", linestyle='--', alpha=0.5)
 
     fig, ax = plt.subplots()
-    ax.plot(scat[0, :],scat[1,:],',k',alpha=0.5)
+    ax.plot(scat[0, :], scat[1, :], ',k', alpha=0.5)
 
     fig, ax = plt.subplots()
-    ax.hist(scat[1,:], bins=1000)
-
+    ax.hist(scat[1, :], bins=1000)
 
     plt.show()
-
